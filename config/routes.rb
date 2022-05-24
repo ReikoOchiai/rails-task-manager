448f27551bb4cb8bcb840afa26de8c7471a8524d
Reiko Ochiai <lissage.23@gmail.com>
2022-05-24 15:17:52 +1000

    created CRUD functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
+  # create new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # Edit task
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update", as: :task_update
+
+  # delete task
+  delete "tasks/:id", to: "tasks#destroy", as: :task_delete
+  # see specific task
+  get "tasks/:id", to: "tasks#show", as: :task
 end
